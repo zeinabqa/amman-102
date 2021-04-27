eb9d7500dbbcadcef90b40f3757f73854b93a2b1
zeinabqa <zina.qatr@gmail.com>
2021-04-27 15:45:31 +0300

    demo

---
+++ app.js/app.js
@@ -0,0 +1,15 @@
+let color = prompt('Tell Me Your favourite color ');
+console.log(color);
+
+let design = prompt(' what is your favourite design?');
+console.log(design);
+
+let striking = prompt('What is the most striking thing about design ');
+console.log(striking );
+
+let style = prompt('What style do you like to adopt in the decor?');
+console.log(style);
+
+
+
+alert(' your favourite color ' + design + ' your strinking is ' +  ' and your style of your decor '  );
